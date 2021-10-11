00643f29a8a9e28d0726e0e5182d59dca5476d4a
Sabeany <sabineborovika@gmail.com>
2021-10-11 21:39:00 +0300

    Second commit to BeanBranch

--- StudentName/src/StudentName.java
+++ StudentName/src/StudentName.java
@@ -1,6 +1,9 @@
 public class StudentName {
 
     public static void main(String[] args) {
+       toMerge();
+    }
+    private static void toMerge() {
         System.out.println("Hello World!");
     }
 }
