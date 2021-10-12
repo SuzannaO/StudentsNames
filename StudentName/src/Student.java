074dbafaf76ce86c2360a5422658014b8d5580d6
SuzannaO <ostrovskaya.s3@gmail.com>
2021-10-12 17:46:30 +0300

    New Name

---
+++ StudentName/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+        public static void main(String[] args) {
+            name();
+        }
+        public static void name()
+        {
+            System.out.println("My name is Susan");
+        }
+    }
+
